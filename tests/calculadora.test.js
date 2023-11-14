33139e13e9d361e594e41961685571982865a22b
Ricardo Silva <47871470+ricardo2009@users.noreply.github.com>
2023-11-14 14:02:43 +0000

    `testes´

--- tests/calculadora.test.js
+++ tests/calculadora.test.js
@@ -1,11 +1,16 @@
-test("espero que 1 seja 1", () => {
-  expect(1).toBe(1); // cenario de teste PASS
-  // expect(2).toBe(1); // cenario de teste FAIL
-  // expect = valor gerado  dinamicamente
+const calculadora = require("../models/calculadora.js");
 
-  // o valor declaro no expect é declarado no toBe
-  // toBe = valor esperado
-  // o valor que vem computado no sistema de forma dinamica é declarado no expect
+test("somar 2 +2 deveria retornar 4", () => {
+  const resultado = calculadora.somar(2, 2);
+  expect(resultado).toBe(4);
+});
 
-  // por exemplo em um sistema de calculadora onde o valor esperado é 1 em um cenario mais por algum motivo o valor computado é 2, o teste vai falhar!
+test("somar 5 + 100 deveria retornar 105", () => {
+  const resultado = calculadora.somar(5, 100);
+  expect(resultado).toBe(105);
+});
+
+test("somar 'banana' + 100 deveria retornar 'Error'", () => {
+  const resultado = calculadora.somar("banana", 100);
+  expect(resultado).toBe("Error");
 });
