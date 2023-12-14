bb8cf84479c437253e93d60d873c9d284f46b20c
Ricardo Silva <47871470+ricardo2009@users.noreply.github.com>
2023-12-14 12:51:14 +0000

    avançando com o projeto

--- config.sh
+++ config.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
-sudo apt-get update
+sudo apt-get update -y
+sudo apt-get upgrade -y
+sudo apt-get install -y build-essential
+sudo apt-get install -y curl
+sudo apt-get install -y wget
 sudo apt install dnsutils -y 
 sudo apt install net-tools -y
