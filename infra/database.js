83c01d8bf622464c5617b4f43ea6cf877fa6804c
Ricardo Silva <47871470+ricardo2009@users.noreply.github.com>
2024-02-06 12:42:30 +0000

    add `database.js` and `.env`´

--- infra/database.js
+++ infra/database.js
@@ -5,9 +5,10 @@ async function query(queryObject) {
     host: process.env.POSTGRES_HOST,
     port: process.env.POSTGRES_PORT,
     user: process.env.POSTGRES_USER,
-    database: process.env.POSTGRES_DATABASE,
+    database: process.env.POSTGRES_DB,
     password: process.env.POSTGRES_PASSWORD,
   });
+
   await client.connect();
   const result = await client.query(queryObject);
   await client.end();
