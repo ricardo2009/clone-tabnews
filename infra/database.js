b1c6dcd0346d5aa6de966426840a30b7b2dbeab4
Ricardo Silva <47871470+ricardo2009@users.noreply.github.com>
2024-08-02 17:37:24 +0000

    handles database ssl connection

--- infra/database.js
+++ infra/database.js
@@ -7,6 +7,7 @@ async function query(queryObject) {
     user: process.env.POSTGRES_USER,
     database: process.env.POSTGRES_DB,
     password: process.env.POSTGRES_PASSWORD,
+    ssl: process.env.NODE_ENV === "development" ? false : true,
   });
   console.log('Credenciais do Postgres: ', {
     host: process.env.POSTGRES_HOST,
@@ -15,20 +16,18 @@ async function query(queryObject) {
     database: process.env.POSTGRES_DB,
     password: process.env.POSTGRES_PASSWORD,
   });
-  
-  
 
   try {
-   await client.connect();
-   const result = await client.query(queryObject);
-   return result;
+    await client.connect();
+    const result = await client.query(queryObject);
+    return result;
   } catch (error) {
     console.error(error);
     throw error;
   } finally {
     await client.end();
   }
-  
+
 
 }
 
