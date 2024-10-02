c1dd4b08ea847a52f6db47a5cad87123cc9ccc10
Ricardo Silva <47871470+ricardo2009@users.noreply.github.com>
2024-10-02 14:28:00 +0000

    refactor: improve SSL configuration handling in database connection

--- infra/database.js
+++ infra/database.js
@@ -7,14 +7,7 @@ async function query(queryObject) {
     user: process.env.POSTGRES_USER,
     database: process.env.POSTGRES_DB,
     password: process.env.POSTGRES_PASSWORD,
-    ssl: process.env.NODE_ENV === "development" ? false : true,
-  });
-  console.log('Credenciais do Postgres: ', {
-    host: process.env.POSTGRES_HOST,
-    port: process.env.POSTGRES_PORT,
-    user: process.env.POSTGRES_USER,
-    database: process.env.POSTGRES_DB,
-    password: process.env.POSTGRES_PASSWORD,
+    ssl: getSSLValues(),
   });
 
   try {
@@ -27,10 +20,18 @@ async function query(queryObject) {
   } finally {
     await client.end();
   }
-
-
 }
 
 export default {
   query: query,
+
 };
+
+function getSSLValues() {
+  if (process.env.POSTGRES_CA) {
+    return {
+      ca: process.env.POSTGRES_CA
+    }
+  }
+  return process.env.NODE_ENV === "development" ? false : true
+}
